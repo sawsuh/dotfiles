b7c3d3a75f74127f8e7e3fdb5da136bde4ffd86d
sawsuh <prao060@gmail.com>
2019-12-25 17:45:48 +1100

    made screenshot script delete files after an hour

--- thinkpad/.scripts/scrrect.sh
+++ thinkpad/.scripts/scrrect.sh
@@ -1,7 +1,5 @@
-#maim -f png -u screenshots/$(date +'%-d-%-m-%-y_%-R').png
 filename=$(date +'%-d-%-m-%-y_%T')
 maim -sul -b 0 -c 0.25882352941176473,0.25882352941176473,0.23921568627450981,0.5 $filename.png
 sleep 1;
 dunstify "screenshot taken"
-sleep 3600;
-rm ~/$filename.png
+at now + 1 hour < <(echo "rm ~/$filename.png")
