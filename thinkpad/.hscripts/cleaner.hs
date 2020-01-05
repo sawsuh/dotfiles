f5f97924fa259ea421c9cf236b08801aa28ac365
sawsuh <prao060@gmail.com>
2020-01-05 12:55:33 +1100

    finalised downloads cleaning haskell script

--- thinkpad/.hscripts/cleaner.hs
+++ thinkpad/.hscripts/cleaner.hs
@@ -3,13 +3,19 @@ import Data.Time.Clock
 import System.Environment
 
 main = do 
-	[directory, days] <- getArgs
+	[directory, ndays] <- getArgs
+	fileremover directory (read ndays :: Integer)
+
+-- removes files using helper function filestoremove
+fileremover :: String -> Integer -> IO ()
+fileremover directory days = do
 	setCurrentDirectory directory
 	files <- listDirectory "."
 	times <- mapM getModificationTime files -- list of file modification times
 	ctime <- getCurrentTime -- current time
-	mapM_ removeFile $ filestoremove files times ctime (read days :: Integer)
+	mapM_ removeFile $ filestoremove files times ctime days
 
+-- works out what files to remove
 filestoremove :: [FilePath] -> [UTCTime] -> UTCTime -> Integer -> [FilePath]
 filestoremove files times ctime days =
 	let fileswithdiffs = zip files $ map (diffUTCTime ctime) times -- [files] -> [(file, how many seconds ago last modified)]
