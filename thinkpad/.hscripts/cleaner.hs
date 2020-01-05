0d24c88d5821625a0f6ec032d658cab6d84e4fbc
sawsuh <prao060@gmail.com>
2020-01-05 13:04:08 +1100

    added comments to downloads cleaner

--- thinkpad/.hscripts/cleaner.hs
+++ thinkpad/.hscripts/cleaner.hs
@@ -6,17 +6,17 @@ main = do
 	[directory, ndays] <- getArgs
 	fileremover directory (read ndays :: Integer)
 
--- removes files using helper function filestoremove
+-- removes files from a directory given a number of days using helper function filestoremove to filter list of files to which files to remove
 fileremover :: String -> Integer -> IO ()
 fileremover directory days = do
-	setCurrentDirectory directory
-	files <- listDirectory "."
+	setCurrentDirectory directory -- goes to directory
+	files <- listDirectory "." -- list of files
 	times <- mapM getModificationTime files -- list of file modification times
 	ctime <- getCurrentTime -- current time
-	mapM_ removeFile $ filestoremove files times ctime days
+	mapM_ removeFile $ filestoremove files times ctime days -- removes files using helper function
 
--- works out what files to remove
+-- works out what files to remove by getting the ones that were modified more than a given number of seconds/days ago (86400 seconds in a day) 
 filestoremove :: [FilePath] -> [UTCTime] -> UTCTime -> Integer -> [FilePath]
 filestoremove files times ctime days =
 	let fileswithdiffs = zip files $ map (diffUTCTime ctime) times -- [files] -> [(file, how many seconds ago last modified)]
-	in [f | (f,t) <- fileswithdiffs, t > (fromInteger $ ( days ) * 86400) ]
+	in [f | (f,t) <- fileswithdiffs, t > (fromInteger $ ( days ) * 86400) ] -- [(files, how many seconds ago last modified)] -> [files] filtered by last modified at least n days or 86400 * n seconds ago
