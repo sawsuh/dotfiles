75d7e1d5dfeab8ea986ea37d7463b3ee562cee6f
sawsuh <prao060@gmail.com>
2020-02-15 11:22:01 +1100

    added music scraper thats better

---
+++ thinkpad/.pyscripts/musicscrape/run.py
@@ -0,0 +1,30 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+import json
+import os
+import dateparser
+
+process = CrawlerProcess(get_project_settings())
+
+process.crawl("nd")
+process.crawl("p4k")
+process.start()
+
+with open("~/.music.json") as f:
+    data = []
+    for line in f:
+        data.append(json.loads(line))
+
+clean = []
+for line in data:
+    date = dateparser.parse(line["date"])
+    title = line["title"]
+    artist = line["artist"]
+    entrydate = f'{date.strftime("%d %b %Y")}'
+    album = f'{artist} - {title}'
+    clean.append((f"{album:<80} {entrydate}", date))
+
+for item, date in sorted(clean, key=lambda x: x[1], reverse=True):
+    print(item)
+
+os.remove("~/.music.json")
