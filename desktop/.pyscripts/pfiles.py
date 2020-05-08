c9d0eaed9333e4fa000679e4e08b6024a46c296d
sawsuh <prao060@gmail.com>
2020-05-08 17:35:58 +1000

    updated vifm stuff

--- desktop/.pyscripts/pfiles.py
+++ desktop/.pyscripts/pfiles.py
@@ -11,7 +11,7 @@ def files(x, y):
             "-h",
             "600",
             "-w",
-            "1200",
+            "900",
             "-x",
             str(x),
             "-y",
@@ -19,7 +19,7 @@ def files(x, y):
             "-n",
             "1",
             "-f",
-            "-name stick1 -e ranger",
+            "-name stick1 -e vifm",
             "urxvt",
         ]
     )
@@ -32,11 +32,11 @@ right = bar("pop2", "HDMI-A-0")
 
 if xcord > 2560:
     if right.barvisible():
-        files(3902, 752)
+        files(4202, 752)
     else:
-        files(3902, 822)
+        files(4202, 822)
 else:
     if left.barvisible():
-        files(1342, 752)
+        files(1642, 752)
     else:
-        files(1342, 822)
+        files(1642, 822)
