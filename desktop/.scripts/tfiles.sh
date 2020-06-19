44f1afc5d447a883633d5bf9abb0112202adb838
sawsuh <prao060@gmail.com>
2020-06-20 01:05:52 +1000

    updated script to fix

--- desktop/.scripts/tfiles.sh
+++ desktop/.scripts/tfiles.sh
@@ -3,5 +3,5 @@ eval $(xdotool getmouselocation --shell)
 if (( $X > 2560 )); then
     tdrop -a -h 600 -w 1100 -x 3992 -y 742 -n 1 -f "--name stick1 -e ranger" kitty
 else
-    tdrop -a -h 600 -w 1100 -x 3992 -y 742 -n 1 -f "--name stick1 -e ranger" kitty
+    tdrop -a -h 600 -w 1100 -x 1432 -y 742 -n 1 -f "--name stick1 -e ranger" kitty
 fi
