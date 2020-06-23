ef3a0afee484452c1210fd3e3286198b13f8a2ed
sawsuh <prao060@gmail.com>
2020-06-23 16:12:26 +1000

    few more sway qol changes

--- thinkpad/.scripts/files.sh
+++ thinkpad/.scripts/files.sh
@@ -1,6 +1,1 @@
-if [[ -n $1 ]]
-then
-    tdrop -a -h 500 -w 850 -x 1054 -y 564 -n 1 -f "--class stick1 -o font_size=13 -e ranger" kitty	
-else
-    tdrop -a -h 500 -w 850 -x 1054 -y 564 -n 1 -f "--name stick1 -e ranger" kitty	
-fi
+tdrop -a -h 500 -w 850 -x 1054 -y 564 -n 1 -f "--name stick1 -e ranger" kitty	
