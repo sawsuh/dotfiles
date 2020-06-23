18d3055a9f7d899f1070215432ad877b111c2731
sawsuh <prao060@gmail.com>
2020-06-23 16:10:28 +1000

    more updates to sway stuff

--- thinkpad/.scripts/files.sh
+++ thinkpad/.scripts/files.sh
@@ -1,1 +1,6 @@
-tdrop -a -h 500 -w 850 -x 1054 -y 564 -n 1 -f "--name stick1 -e ranger" kitty	
+if [[ -n $1 ]]
+then
+    tdrop -a -h 500 -w 850 -x 1054 -y 564 -n 1 -f "--class stick1 -o font_size=13 -e ranger" kitty	
+else
+    tdrop -a -h 500 -w 850 -x 1054 -y 564 -n 1 -f "--name stick1 -e ranger" kitty	
+fi
