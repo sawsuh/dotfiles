d591425c18617b7fd8c588aaf48ca6e017f473cc
sawsuh <prao060@gmail.com>
2020-07-03 00:23:19 +1000

    updated scripts to have 928374 as the selection color

--- desktop/.scripts/swayscrrect.sh
+++ desktop/.scripts/swayscrrect.sh
@@ -1,5 +1,5 @@
 filename=$(date +'%y-%-m-%-d_%-T')
-grim -g "$(slurp -b 28282888 -s ebdbb288)" $filename.png
+grim -g "$(slurp -b 28282888 -s 928374)" $filename.png
 sleep 1;
 notify-send "screenshot taken"
 at now + 1 hour < <(echo "rm ~/$filename.png")
