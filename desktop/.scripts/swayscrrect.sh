1498a4e2a7e49079e8c005f8d806a90577e966c7
sawsuh <prao060@gmail.com>
2020-07-03 00:24:15 +1000

    added selection opacity to screenshot scripts

--- desktop/.scripts/swayscrrect.sh
+++ desktop/.scripts/swayscrrect.sh
@@ -1,5 +1,5 @@
 filename=$(date +'%y-%-m-%-d_%-T')
-grim -g "$(slurp -b 28282888 -s 928374)" $filename.png
+grim -g "$(slurp -b 28282888 -s 92837488)" $filename.png
 sleep 1;
 notify-send "screenshot taken"
 at now + 1 hour < <(echo "rm ~/$filename.png")
