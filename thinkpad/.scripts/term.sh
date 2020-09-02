8f10e2875fa2e646c8db41ca37c34d426a392f67
sawsuh <prao060@gmail.com>
2020-09-02 18:14:14 +1000

    updated drawterm script :)

--- thinkpad/.scripts/term.sh
+++ thinkpad/.scripts/term.sh
@@ -2,15 +2,25 @@
 dims=$(slop -t 0 -b 0 -c "0.25882352941176473,0.25882352941176473,0.23921568627450981,0.5" -l -f "%w %h %x %y" --nokeyboard)
 IFS=' ' read w h x y <<< $dims
 
-kitty --name float& 
+kitty -o initial_window_width=$w -o initial_window_height=$h -o remember_window_size=false --name float& 
 sleep 0.5
 
-wid=$(xdotool getactivewindow)
-xdotool windowsize $wid $w $h
+windows=$(xdotool search --desktop=$(xdotool get_desktop) --class .)
+wid=''
+for win in $windows
+do
+    width=$(xdotool getwindowgeometry $win | sed -n 3p | awk -F '[:|x]' '{print $2}')
+    height=$(xdotool getwindowgeometry $win | sed -n 3p | awk -F '[:|x]' '{print $3}')
+    if [ "$width" -eq "$w" ] && [ "$height" -eq "$h" ];
+    then
+        wid="$win"
+    fi
+done
+
 xdotool windowmove $wid $x $y
 (( midx= x + (w/2) ))
 (( midy= y + (h/2) ))
 
 sleep 0.2
 xdotool mousemove $midx $midy
-dotool windowactivate $wid
+xdotool windowactivate $wid
