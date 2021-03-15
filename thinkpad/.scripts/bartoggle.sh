140c5cb40af8770393a2791b7981943b0231bcdb
Prashant Rao <prao060@gmail.com>
2021-03-15 14:23:51 +1100

    made a good bartoggle script

--- thinkpad/.scripts/bartoggle.sh
+++ thinkpad/.scripts/bartoggle.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+spawnbar () {
+    bspc config top_padding 70
+    polybar barski&>/dev/null &
+    exit 0
+}
+mapbar () {
+    bspc config top_padding 70
+    xdotool windowmap $1
+    exit 0
+}
+unmapbar() {
+    xdotool windowunmap $1
+    bspc config top_padding 20
+} 
 pid=$(pgrep -a polybar | grep barski | awk '{print $1}')
-# spawns bar if bar process doesn't exist then exits
-[[ -z $pid ]] && { bspc config top_padding 70; { polybar barski&>/dev/null ; } & exit 0; }
+[[ -z $pid ]] && spawnbar
 wid=$(xdotool search --pid $pid --onlyvisible)
-# maps bar if bar wid doesn't exist but pid does
-# (no bar is drawn but process exists) then exits
-[[ -z $wid ]] && { bspc config top_padding 70; xdotool windowmap "$(xdotool search --pid $pid)"; exit 0; }
-# unmaps bar if bar is drawn 
-xdotool windowunmap $wid
-bspc config top_padding 20
+[[ -z $wid ]] && { mapbar "$(xdotool search --pid $pid)"; }
+unmapbar $wid
