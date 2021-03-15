b87510df81b333b1ce122c6eea460b474c021cee
sawsuh <prao060@gmail.com>
2021-03-15 18:26:15 +1100

    made bar script simpler

--- thinkpad/.scripts/bartoggle.sh
+++ thinkpad/.scripts/bartoggle.sh
@@ -12,13 +12,7 @@ unmapbar() {
     bspc config top_padding 20
 } 
 pid=$(pgrep -a polybar | grep barski | awk '{print $1}')
-<<<<<<< HEAD
-[[ -z $pid ]] && spawnbar
 wid=$(xdotool search --pid $pid --onlyvisible)
-[[ -z $wid ]] && mapbar "$(xdotool search --pid $pid)"
-unmapbar $wid
-=======
-[[ -z $pid ]] && { spawnbar; exit 0; }
-[[ -z $(xdotool search --pid $pid --onlyvisible) ]] && { mapbar; exit 0; }
 unmapbar
->>>>>>> 1e9ffb98b500da95078aab78a0ae4882516fcace
+[[ -z $pid ]] && spawnbar
+[[ -z $wid ]] && mapbar
