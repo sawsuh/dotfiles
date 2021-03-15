1e9ffb98b500da95078aab78a0ae4882516fcace
Prashant Rao <prao060@gmail.com>
2021-03-15 17:38:48 +1100

    made bartoggle a bit nicer

--- thinkpad/.scripts/bartoggle.sh
+++ thinkpad/.scripts/bartoggle.sh
@@ -2,19 +2,16 @@
 spawnbar () {
     bspc config top_padding 70
     polybar barski&>/dev/null &
-    exit 0
 }
 mapbar () {
     bspc config top_padding 70
-    xdotool windowmap $1
-    exit 0
+    polybar-msg cmd show
 }
 unmapbar() {
-    xdotool windowunmap $1
+    polybar-msg cmd hide
     bspc config top_padding 20
 } 
 pid=$(pgrep -a polybar | grep barski | awk '{print $1}')
-[[ -z $pid ]] && spawnbar
-wid=$(xdotool search --pid $pid --onlyvisible)
-[[ -z $wid ]] && { mapbar "$(xdotool search --pid $pid)"; }
-unmapbar $wid
+[[ -z $pid ]] && { spawnbar; exit 0; }
+[[ -z $(xdotool search --pid $pid --onlyvisible) ]] && { mapbar; exit 0; }
+unmapbar
