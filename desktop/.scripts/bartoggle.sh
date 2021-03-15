f987745c610eb5b01cdb39e266c6f2f78d567462
sawsuh <prao060@gmail.com>
2021-03-16 00:36:52 +1100

    removed overly obvious comments

--- desktop/.scripts/bartoggle.sh
+++ desktop/.scripts/bartoggle.sh
@@ -6,20 +6,20 @@ else
     mon="DP-2"
     cmd="polybar bar1"
 fi
-barspawn () { # $1 = monitor, $2 = command, spawn bar on current monitor
+barspawn () {
     bspc config -m $1 bottom_padding 80
     eval $2&>/dev/null &
 }
-barunmap () { # unmap bar from all monitors and lower all monitors gaps
+barunmap () {
     polybar-msg cmd hide
     xrandr -q | sed -n "s/^\(.*\) connected.*/\1/p" | xargs -I % bspc config -m % bottom_padding 10
 }
-barmap () { # $1 = monitor, $2 = pid, map bar on current monitor
+barmap () { 
     bspc config -m $1 bottom_padding 80
     polybar-msg -p $2 cmd show
 }
 pid=$(pgrep -f "$cmd")
 wid=$(xdotool search --pid $pid --onlyvisible)
 barunmap
-[[ -z $pid ]] && barspawn $mon "$cmd" # if bar not running on current monitor
+[[ -z $pid ]] && barspawn $mon "$cmd" 
 [[ -z $wid ]] && barmap $mon $pid
