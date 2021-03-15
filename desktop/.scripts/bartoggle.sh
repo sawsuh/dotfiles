0ad30c6a7f398e2ec97964f945e8f94619af8c5d
sawsuh <prao060@gmail.com>
2021-03-15 17:29:43 +1100

    added comments to desktop bartoggle

--- desktop/.scripts/bartoggle.sh
+++ desktop/.scripts/bartoggle.sh
@@ -6,25 +6,26 @@ else
     mon="DP-2"
     cmd="polybar bar1"
 fi
-barspawn () { # $1 = monitor, $2 = command
+barspawn () { # $1 = monitor, $2 = command, spawn bar on current monitor
     bspc config -m $1 bottom_padding 80
     eval $2&>/dev/null &
 }
-barunmap () {
-    for pidl in $(pgrep -a polybar | awk '{print $1}')
+barunmap () { # unmap bar from all monitors and lower all monitors gaps
+    for process in $(pgrep -a polybar | awk '{print $1}')
     do
-        polybar-msg -p $pidl cmd hide
+        polybar-msg -p $process cmd hide
     done
     for monitor in $(xrandr -q | grep ' connected' | awk '{print $1}')
     do 
         bspc config -m $monitor bottom_padding 10
     done
 }
-barmap () { # $1 = monitor, $2 = pid
+barmap () { # $1 = monitor, $2 = pid, map bar on current monitor
     bspc config -m $1 bottom_padding 80
     polybar-msg -p $2 cmd show
 }
 pid=$(pgrep -f "$cmd")
-[[ -z $pid ]] && { barunmap; barspawn $mon "$cmd"; exit 0;}
+[[ -z $pid ]] && { barunmap; barspawn $mon "$cmd"; exit 0;} # if bar not running on current monitor
 [[ -z $(xdotool search --pid $pid --onlyvisible) ]] && { barunmap; barmap $mon $pid; exit 0;}
-barunmap; exit 0
+    # if bar running on current monitor but not mapped
+barunmap; exit 0 # if bar mapped on current monitor
