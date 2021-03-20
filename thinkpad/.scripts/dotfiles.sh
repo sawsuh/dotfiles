bb3fcf877a68be51a7b645fb2e7bc49687f11779
Prashant Rao <prao060@gmail.com>
2021-03-21 01:25:39 +1100

    updated dotfiles script better

--- thinkpad/.scripts/dotfiles.sh
+++ thinkpad/.scripts/dotfiles.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-cd $2
-for entry in $(find . | cut -d"/" -f 2-); do
-    path=$(echo "$(pwd)/$entry")
-    dest=$(echo "$3/$entry")
-    [[ $1 == "r" ]] && { [[ -f $path ]] && { [[ $(readlink -f $dest) == $path ]] && { echo "deleting $dest"; rm $dest; }; }; }
-    [[ $1 == "s" ]] && { [[ -d "$path" ]] && { [[ -d "$dest" ]] || { echo "creating folder $dest"; mkdir $dest; }; }; }
-    [[ $1 == "s" ]] && { [[ -f "$path" ]] && { [[ -f "$dest" ]] || { echo "creating symlink $dest to $path"; ln -s $path $dest; }; }; }
-done
+if [[ $1 == "s" ]]; then
+    cd $2
+    find . -path "./.*" | while IFS='' read -r path
+    do
+        [[ -d $path ]] && mkdir -pv $HOME${path#.}
+        [[ -f $path ]] && ln -sv $(pwd)${path#.} $HOME${path#.}
+    done
+elif [[ $1 == "r" ]]; then
+    find -P $HOME -type l | while IFS='' read -r path
+    do 
+        [[ $(readlink -f $path) == "$(realpath $2)${path#$HOME}" ]] && rm -v $path
+    done
+fi
