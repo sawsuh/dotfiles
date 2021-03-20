134bc27be58b6fde15815a468ef49f991409af9f
Prashant Rao <prao060@gmail.com>
2021-03-21 01:54:36 +1100

    updated dotfiles script

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-if [[ $1 == "s" ]]; then
-    cd $2
+if [[ $2 == "s" ]]; then
+    cd $1
     find . -path "./*" | while IFS='' read -r path
     do
         [[ -d $path ]] && mkdir -pv $HOME${path#.}
         [[ -f $path ]] && ln -sv $(pwd)${path#.} $HOME${path#.}
     done
-elif [[ $1 == "r" ]]; then
+elif [[ $2 == "r" ]]; then
     find -P $HOME -type l | while IFS='' read -r path
     do 
-        [[ $(readlink -f $path) == "$(realpath $2)${path#$HOME}" ]] && rm -v $path
+        [[ $(readlink -f $path) == "$(realpath $1)${path#$HOME}" ]] && rm -v $path
     done
 fi
