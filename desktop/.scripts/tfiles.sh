fb51d9eea345b8fcd1e3cf22afa2abf25d79ba5b
sawsuh <prao060@gmail.com>
2021-03-21 13:10:47 +1100

    updated files script

--- desktop/.scripts/tfiles.sh
+++ desktop/.scripts/tfiles.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+barwid () {
+    xdotool search --pid $(pgrep -f "polybar $1") --onlyvisible
+}
+makewin () {
+    tdrop -a -h 600 -w 1100 -x $1 -y $2 -n 1 -f "--name stick1 -e ranger" kitty
+}
 eval $(xdotool getmouselocation --shell)
+y=752
+x=1442
 if (( $X > 2560 )); then
-    tdrop -a -h 600 -w 1100 -x 4002 -y 752 -n 1 -f "--name stick1 -e ranger" kitty
-else
-    tdrop -a -h 600 -w 1100 -x 1442 -y 752 -n 1 -f "--name stick1 -e ranger" kitty
+    if [[ -z "$(barwid 'bar2')" ]]; then
+        y=822
+        x=4002
+    else
+        x=4002
+    fi
+elif [[ -z "$(barwid 'bar1')" ]]; then
+    y=822
 fi
+makewin $x $y
