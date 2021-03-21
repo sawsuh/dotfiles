71831ece6e9d7c0c98520368237eb084cf954a3c
sawsuh <prao060@gmail.com>
2021-03-21 12:55:45 +1100

    updated dotfiles script to be even nicer

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-if [[ $2 == "s" ]]; then
-    cd $1
-    find . -path "./*" | while IFS='' read -r path
-    do
-        [[ -d $path ]] && mkdir -pv $HOME${path#.}
-        [[ -f $path ]] && ln -srv $(pwd)${path#.} $HOME${path#.}
-    done
-elif [[ $2 == "r" ]]; then
-    find -P $HOME -type l | while IFS='' read -r path
-    do 
-        [[ $(readlink -f $path) == "$(realpath $1)"* ]] && rm -v $path
-    done
-fi
+cd $1
+case $2 in
+    "s")
+        find . -path "./*" | while IFS='' read -r path; do
+            [[ -d $path ]] && mkdir -pv $HOME${path#.}
+            [[ -f $path ]] && ln -srv $path $HOME${path#.}
+        done;;
+    "r")
+        find -P $HOME -type l | while IFS='' read -r path; do
+            [[ $(readlink -f $path) == "$(pwd)"* ]] && rm -v $path
+        done;;
+esac
