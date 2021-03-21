f8110e417b2ff5c94ed093f8912c3d9ca31eebae
sawsuh <prao060@gmail.com>
2021-03-21 20:50:24 +1100

    updated polybar script even more

--- desktop/.scripts/bartoggle.sh
+++ desktop/.scripts/bartoggle.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
-if [[ $(xdotool getmouselocation | awk -F '[ :]' '{print $2}') -gt 2560 ]]; then
-    mon="DP-0"
-    cmd="polybar bar2"
-else
-    mon="DP-2"
-    cmd="polybar bar1"
-fi
-barspawn () {
-    bspc config -m $1 bottom_padding 80
-    eval $2&>/dev/null &
-}
-barunmap () {
-    polybar-msg cmd hide
-    xrandr -q | awk '/ connected/{print $1}' | xargs -I % bspc config -m % bottom_padding 10
-}
-barmap () { 
-    bspc config -m $1 bottom_padding 80
-    polybar-msg -p $2 cmd show
-}
-pid=$(pgrep -f "$cmd")
+mon="DP-2"
+cmd="bar1"
+[[ $(xdotool getmouselocation | awk -F '[ :]' '{print $2}') -gt 2560 ]] && { mon="DP-0"; cmd="bar2"; }
+pid=$(pgrep -f "polybar $cmd")
 wid=$(xdotool search --pid $pid --onlyvisible)
-barunmap
-[[ -z $pid ]] && barspawn $mon "$cmd" 
-[[ -z $wid ]] && barmap $mon $pid
+polybar-msg cmd hide
+bspc config bottom_padding 10
+[[ -z $pid ]] && { bspc config -m $mon bottom_padding 80; eval "polybar $cmd"&>/dev/null& exit 0; }
+[[ -z $wid ]] && { bspc config -m $mon bottom_padding 80; polybar-msg -p $pid cmd show; }
