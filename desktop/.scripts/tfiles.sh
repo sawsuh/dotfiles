9dedfee91a4e946fca73ee3e0635a4784c8c62ae
Prashant Rao <prao060@gmail.com>
2021-03-21 15:58:04 +1100

    simplified files script

--- desktop/.scripts/tfiles.sh
+++ desktop/.scripts/tfiles.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-barwid () {
-    xdotool search --pid $(pgrep -f "polybar $1") --onlyvisible
-}
 eval $(xdotool getmouselocation --shell)
 y=752
 x=1442
-if (( $X > 2560 )); then
-    if [[ -z "$(barwid 'bar2')" ]]; then
-        y=822
-        x=4002
-    else
-        x=4002
-    fi
-elif [[ -z "$(barwid 'bar1')" ]]; then
-    y=822
-fi
+b='bar1'
+(( $X > 2560 )) && { x=4002; b='bar2'; }
+[[ -z "$(xdotool search --pid $(pgrep -f \"polybar $b\") --onlyvisible)" ]] && y=822
 tdrop -a -h 600 -w 1100 -x $x -y $y -n 1 -f "--name stick1 -e ranger" kitty
