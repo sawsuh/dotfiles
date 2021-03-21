c738647362f8e09f99deb8d51823dfd498c29a66
sawsuh <prao060@gmail.com>
2021-03-21 17:54:34 +1100

    updated files script

--- desktop/.scripts/tfiles.sh
+++ desktop/.scripts/tfiles.sh
@@ -4,5 +4,5 @@ y=752
 x=1442
 b='bar1'
 (( $X > 2560 )) && { x=4002; b='bar2'; }
-[[ -z "$(xdotool search --pid $(pgrep -f \"polybar $b\") --onlyvisible)" ]] && y=822
+[[ -z $(xdotool search --pid $(pgrep -f "polybar $b") --onlyvisible) ]] && y=822
 tdrop -a -h 600 -w 1100 -x $x -y $y -n 1 -f "--name stick1 -e ranger" kitty
