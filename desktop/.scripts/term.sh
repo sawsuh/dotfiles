84619e02359fa3525efeeaebca0bf35a091ef032
Prashant Rao <prao060@gmail.com>
2021-03-22 13:05:51 +1100

    adjusted desktop term script for borders

--- desktop/.scripts/term.sh
+++ desktop/.scripts/term.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 IFS=' ' read w h x y < <(slop -t 0 -b 0 -c "0.25882352941176473,0.25882352941176473,0.23921568627450981,0.5" -l -f "%w %h %x %y" --nokeyboard)
-(( w -= 6 ))
-(( h -= 6 ))
+(( w -= 8 ))
+(( h -= 8 ))
 kitty -o initial_window_width=$w -o initial_window_height=$h -o remember_window_size=false --name float& 
 while [ : ]; do
     for win in $(xdotool search --desktop=$(xdotool get_desktop) --class .); do
