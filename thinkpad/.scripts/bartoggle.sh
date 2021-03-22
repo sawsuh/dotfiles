9b8728d6f6d17a1fc5dc372bede7f9ea5fdaf887
Prashant Rao <prao060@gmail.com>
2021-03-22 13:08:57 +1100

    added bartoggle script

--- thinkpad/.scripts/bartoggle.sh
+++ thinkpad/.scripts/bartoggle.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 cmd="polybar barski"
-spawnbar () {
-    bspc config top_padding 70
-    eval "$cmd"&>/dev/null &
-}
-mapbar () {
-    bspc config top_padding 70
-    polybar-msg cmd show
-}
-unmapbar() {
-    polybar-msg cmd hide
-    bspc config top_padding 20
-} 
 pid=$(pgrep -f "$cmd")
 wid=$(xdotool search --pid $pid --onlyvisible)
-unmapbar
-[[ -z $pid ]] && spawnbar
-[[ -z $wid ]] && mapbar
+polybar-msg cmd hide
+bspc config top_padding 20
+[[ -z $pid ]] && eval "$cmd"&>/dev/null &
+[[ -z $wid ]] && { bspc config top_padding 70; polybar-msg cmd show; }
