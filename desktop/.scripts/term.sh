69cde839ecc3d22a0521f47a3dd1ad50234c90bb
Prashant Rao <prao060@gmail.com>
2021-03-22 13:05:12 +1100

    made drawterm script way better

--- desktop/.scripts/term.sh
+++ desktop/.scripts/term.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-dims=$(slop -t 0 -b 0 -c "0.25882352941176473,0.25882352941176473,0.23921568627450981,0.5" -l -f "%w %h %x %y" --nokeyboard)
-IFS=' ' read w h x y <<< $dims
-
-kitty --name float& 
-sleep 0.5
-
-wid=$(xdotool getactivewindow)
-xdotool windowsize $wid $w $h
+IFS=' ' read w h x y < <(slop -t 0 -b 0 -c "0.25882352941176473,0.25882352941176473,0.23921568627450981,0.5" -l -f "%w %h %x %y" --nokeyboard)
+(( w -= 6 ))
+(( h -= 6 ))
+kitty -o initial_window_width=$w -o initial_window_height=$h -o remember_window_size=false --name float& 
+while [ : ]; do
+    for win in $(xdotool search --desktop=$(xdotool get_desktop) --class .); do
+        eval "$(xdotool getwindowgeometry --shell $win)"
+        [ "$WIDTH" -eq "$w" ] && [ "$HEIGHT" -eq "$h" ] && { wid="$win"; break 2; }
+    done
+done
 xdotool windowmove $wid $x $y
-(( midx= x + (w/2) ))
-(( midy= y + (h/2) ))
-
-sleep 0.2
-xdotool mousemove $midx $midy
-dotool windowactivate $wid
+xdotool mousemove $(( x + (w/2) )) $(( y + (h/2) ))
+xdotool windowactivate $wid
