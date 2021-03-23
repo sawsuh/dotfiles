ed467917abbd48a37e558f9dfb2405bcdee464e2
Prashant Rao <prao060@gmail.com>
2021-03-23 16:21:49 +1100

    updated dotfiles script

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,12 +2,12 @@
 cd $1
 case $2 in
     "s")
-        find . -path "./*" | while IFS='' read -r path; do
+        find . -path "./*" | while IFS= read -r path; do
             [[ -d $path ]] && mkdir -pv $HOME${path#.}
             [[ -f $path ]] && ln -srv $path $HOME${path#.}
         done;;
     "r")
-        find -P $HOME -type l | while IFS='' read -r path; do
+        find -P $HOME -type l | while IFS= read -r path; do
             [[ $(readlink -f $path) == "$(pwd)"* ]] && rm -v $path
         done;;
 esac
