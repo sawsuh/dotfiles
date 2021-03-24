63a8726352e33e387be33b57763c59c69cec4962
Prashant Rao <prao060@gmail.com>
2021-03-24 15:14:10 +1100

    updated script a bit better

--- shared/.scripts/dotfiles.sh
+++ shared/.scripts/dotfiles.sh
@@ -2,7 +2,7 @@
 cd $1
 case $2 in
     "s")
-        find . -P -path "./*" \
+        find -P . -path "./*" \
             \( -type d -exec bash -c 'mkdir -pv "$HOME${0#.}"' {} \; \) -o \
             \( -type f -exec bash -c 'ln -srv "$0" "$HOME${0#.}"' {} \; \) -o \
             \( -type l -exec bash -c 'ln -srv "$(readlink -f $0)" "$HOME${0#.}"' {} \; \);;
