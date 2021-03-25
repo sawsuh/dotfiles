0f503cd7b37cc9258ea02c84904ebfc5a72b13c3
Prashant Rao <prao060@gmail.com>
2021-03-25 14:31:21 +1100

    made dots script better at following links

--- shared/.scripts/dotfiles.sh
+++ shared/.scripts/dotfiles.sh
@@ -4,9 +4,9 @@ case $2 in
     "s")
         find -P . -path "./*" \
             \( -type d -exec bash -c 'mkdir -pv "$HOME${0#.}"' {} \; \) -o \
-            \( -type l,f -exec bash -c 'ln -srv "$0" "$HOME${0#.}"' {} \; \);;
+            \( -type l -exec bash -c 'ln -srv "$(readlink -ef $0)" "$HOME${0#.}"' {} \; \) -o \
+            \( -type f -exec bash -c 'ln -srv "$0" "$HOME${0#.}"' {} \; \);;
     "r")
         find -P $HOME \( -path "$(pwd)" -prune \) -o \
             \( -type l -exec bash -c '[[ $(readlink -f $0) == "$(pwd)"* ]] && rm -v $0' {} \; \);;
 esac
-            #\( -type l -exec bash -c 'ln -srv "$(readlink -ef $0)" "$HOME${0#.}"' {} \; -o \
