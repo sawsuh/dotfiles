ec02bdc818e6b9c3df2a60df8ceaa76eb5bf743e
Prashant Rao <prao060@gmail.com>
2021-03-25 13:07:21 +1100

    updated dotfiles script to copy stow

--- shared/.scripts/dotfiles.sh
+++ shared/.scripts/dotfiles.sh
@@ -4,8 +4,7 @@ case $2 in
     "s")
         find -P . -path "./*" \
             \( -type d -exec bash -c 'mkdir -pv "$HOME${0#.}"' {} \; \) -o \
-            \( -type l -exec bash -c 'ln -srv "$(readlink -f $0)" "$HOME${0#.}"' {} \; \) -o \
-            \( -type f -exec bash -c 'ln -srv "$0" "$HOME${0#.}"' {} \; \);;
+            \( -type l,f -exec bash -c 'ln -srv "$0" "$HOME${0#.}"' {} \; \);;
     "r")
         find -P $HOME \( -path "$(pwd)" -prune \) -o \
             \( -type l -exec bash -c '[[ $(readlink -f $0) == "$(pwd)"* ]] && rm -v $0' {} \; \);;
