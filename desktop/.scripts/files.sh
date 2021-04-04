67417dd685d7ca2ed65b92044a7c254f43d91d51
sawsuh <prao060@gmail.com>
2021-04-04 23:18:33 +1000

    updated files script

--- desktop/.scripts/files.sh
+++ desktop/.scripts/files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 eval $(xdotool getmouselocation --shell)
-y=750
-x=1442
+y=900
+x=1642
 b='bar1'
-(( $X > 2560 )) && { x=4002; b='bar2'; }
-[[ -z $(xdotool search --pid $(pgrep -f "polybar $b") --onlyvisible) ]] && y=822
-tdrop -a -h 600 -w 1100 -x $x -y $y -n 1 -f "--name stick1 -e ranger" kitty
+(( $X > 2560 )) && { x=4202; b='bar2'; }
+[[ -z $(xdotool search --pid $(pgrep -f "polybar $b") --onlyvisible) ]] && y=972
+tdrop -a -h 450 -w 900 -x $x -y $y -n 1 -f "--name stick1 -e ranger" kitty
