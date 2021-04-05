cd01a1e778f121483543026af31436cc0acfbcf6
sawsuh <prao060@gmail.com>
2021-04-06 00:10:33 +1000

    updated term script to be marginally more efficient

--- shared/.scripts/term.sh
+++ shared/.scripts/term.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 IFS=' ' read w h x y < <(slop -t 0 -b 0 -c "0.25882352941176473,0.25882352941176473,0.23921568627450981,0.5" -l -f "%w %h %x %y" --nokeyboard)
 source ~/.scripts/term_width
-(( w -= pw ))
-(( h -= ph ))
+(( w -= pwh ))
+(( h -= pwh ))
 kitty -o initial_window_width=$w -o initial_window_height=$h -o remember_window_size=false --name float& 
 while [ -z "$wid" ]; do
     while IFS= read win; do
