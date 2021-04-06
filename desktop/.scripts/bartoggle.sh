1a40f3eb83772c361a001264a75f4065ceac9c97
sawsuh <prao060@gmail.com>
2021-04-06 18:24:28 +1000

    made bartoggle way easier

--- desktop/.scripts/bartoggle.sh
+++ desktop/.scripts/bartoggle.sh
@@ -5,7 +5,7 @@ eval $(xdotool getmouselocation --shell)
 [[ $X -gt 2560 ]] && { mon="DP-0"; cmd="bar2"; }
 pid=$(pgrep -f "polybar $cmd")
 wid=$(xdotool search --pid $pid --onlyvisible)
-polybar-msg cmd hide
-bspc config bottom_padding 10
-[[ -z $wid ]] && { bspc config -m $mon bottom_padding 80; polybar-msg -p $pid cmd show; }
-[[ -z $pid ]] && eval "polybar $cmd"&>/dev/null&
+polybar-msg cmd hide; bspc config bottom_padding 10
+[[ -z $wid ]] && runcmd=`bspc config -m $mon bottom_padding 80; polybar-msg -p $pid cmd show;`
+[[ -z $pid ]] && runcmd=`eval "polybar $cmd"&>/dev/null&`
+eval $runcmd
