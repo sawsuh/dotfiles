8cf71196cf90dea6984f4d2ff5a23c592069d923
Prashant Rao <prao060@gmail.com>
2021-05-07 03:00:05 +1000

    updated assgen further

--- shared/.hscripts/assgen.hs
+++ shared/.hscripts/assgen.hs
@@ -5,8 +5,8 @@ import System.Directory
 data Conf = Conf { code :: String, na :: Int, nq :: Int }
 
 maintext = do
-    title <- (++) . (++" Assignment ") <$> code <*> fmap show na
-    qList <- foldr1 (++) . map (qgen . show) . flip take [1..] <$> nq
+    title <- (++) . (++" Assignment ") <$> code <*> show . na
+    qList <- foldr1 (++) . map (qgen . show) . (`take` [1..]) <$> nq
     return $ "\\documentclass{article}\n\
         \\\title{" ++ title ++"}\n\
         \\\author{U6104703}\n\
@@ -25,14 +25,14 @@ maintext = do
           \\n\
           \% question " ++ m ++ "\n"
 
-getFolder home = (++) . (++) (home ++ "/school/assignments/") . (++"ass") . drop 4 <$> code <*> fmap show na
+getFolder home = (++) . (++) (home ++ "/school/assignments/") . (++"ass") . drop 4 <$> code <*> show . na
 
 main = do
     c:xs <- getArgs
     case map readMaybe xs of
         [Just a, Just q] -> do
             let inp = Conf c a q
-            f <- flip getFolder inp <$> getHomeDirectory 
+            f <- (`getFolder` inp) <$> getHomeDirectory 
             createDirectory f
             writeFile (f++"/ass.tex") $ maintext inp
         _ -> putStrLn "Error reading question and/or assignment number"
