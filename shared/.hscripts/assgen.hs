7e30ad425e732a6eee1d71d4496ed0fd1ef9e1b4
sawsuh <prao060@gmail.com>
2021-05-07 22:59:18 +1000

    updated haskel templates even further

--- shared/.hscripts/assgen.hs
+++ shared/.hscripts/assgen.hs
@@ -1,55 +1,36 @@
-import Data.List
+import qualified Data.Text as T
 import System.Environment
 import Text.Read
 import System.Directory
 import Control.Monad
 
-data Conf = Conf { code :: String, na :: Int, nq :: Int, pret :: String, postt :: String, preqs :: String, midqs :: String, postqs :: String, end :: String }
-
-maintext = do
-    preamble <- pgen
-    qList <- qlistgen
-    e <- end
-    return $ preamble ++ qList ++ e
-pgen = do
-    t <- title
-    pre <- pret
-    post <- postt
-    return $ pre ++ t ++ post
-title = do
-    course <- code
-    assignment <- na
-    return $ course ++ " Assignment " ++ show assignment
-qlistgen = do
-    n <- nq
-    foldr (++) "" . mapM (qgen . show) [1..n]
-qgen m = do 
-    pre <- preqs
-    mid <- midqs
-    post <- postqs
-    return $ pre ++ m ++ mid ++ m ++ post
-getFolder home = do
-    c <- drop 4 . code
-    s <- show . na
-    return $ home ++ "/school/assignments/" ++ c++"ass"++s
-
-splitter f = foldr condAppend [[]]
-    where condAppend x (a:acc) 
-            | f x = []:a:acc
-            | otherwise = (x:a):acc
-
+data Conf = Conf { code :: String, 
+    na :: Int, 
+    nq :: Int, 
+    templ :: T.Text }
+container = fst . T.break (=='!') . templ
+qskele = T.drop 1 . snd . T.break (=='!') . templ
+title = (.) T.pack $ (++) <$> (++" Assignment ") . code <*> show . na
+qgen n = T.replace (T.pack "@") n . qskele
+qList = T.concat . join (mapM (qgen . T.pack . show) . flip take [1..] . nq)
+maintext = T.replace (T.pack "$") . qList <*> (T.replace (T.pack "@") . title <*> container)
+getFolder h = (++) <$> (++"ass") . (++) (h++"/school/assignments/") . drop 4 . code <*> show . na
+    
 main = do
-    c:xs <- getArgs
-    f <- getHomeDirectory
-    datas <- readFile $ f++"/.hscripts/templates/ass.tex"
-    putStrLn $ "read template from "++f++"/.hscripts/templates/ass.tex"
-    case splitter (=='@') datas of 
-        [pretitle, posttitle, preq, midq, postq, end] -> case map readMaybe xs of 
-            [Just a, Just q] -> do
-                let inp = Conf c a q pretitle posttitle preq midq postq end
-                    fo = getFolder f inp
-                createDirectory fo
-                putStrLn $ "made dir "++fo
-                writeFile (fo++"/ass.tex") $ maintext inp
-                putStrLn $ "wrote "++fo++"/ass.tex"
+    course_code:args <- getArgs
+    home_dir <- getHomeDirectory
+    let template_loc = home_dir ++ "/.hscripts/templates/ass.tex"
+    template <- T.pack <$> readFile template_loc
+    putStrLn $ "read template from "++template_loc
+    case map readMaybe args of 
+        [Just assign_num, Just q_count] -> do
+            let config = Conf course_code assign_num q_count template
+                output_folder = getFolder home_dir config
+                output_file = output_folder++"/ass.tex"
+                output_text = T.unpack $ maintext config
+            createDirectory output_folder
+            putStrLn $ "made dir "++output_folder
+            writeFile output_file output_text
+            putStrLn $ "wrote "++output_file
+            putStr output_text
         _ -> putStrLn "No parse"
---
+++ shared/.hscripts/old_assgen.hs
@@ -0,0 +1,55 @@
+import Data.List
+import System.Environment
+import Text.Read
+import System.Directory
+import Control.Monad
+
+data Conf = Conf { code :: String, na :: Int, nq :: Int, pret :: String, postt :: String, preqs :: String, midqs :: String, postqs :: String, end :: String }
+
+maintext = do
+    preamble <- pgen
+    qList <- qlistgen
+    e <- end
+    return $ preamble ++ qList ++ e
+pgen = do
+    t <- title
+    pre <- pret
+    post <- postt
+    return $ pre ++ t ++ post
+title = do
+    course <- code
+    assignment <- na
+    return $ course ++ " Assignment " ++ show assignment
+qlistgen = do
+    n <- nq
+    foldr (++) "" . mapM (qgen . show) [1..n]
+qgen m = do 
+    pre <- preqs
+    mid <- midqs
+    post <- postqs
+    return $ pre ++ m ++ mid ++ m ++ post
+getFolder home = do
+    c <- drop 4 . code
+    s <- show . na
+    return $ home ++ "/school/assignments/" ++ c++"ass"++s
+
+splitter f = foldr condAppend [[]]
+    where condAppend x (a:acc) 
+            | f x = []:a:acc
+            | otherwise = (x:a):acc
+
+main = do
+    c:xs <- getArgs
+    f <- getHomeDirectory
+    datas <- readFile $ f++"/.hscripts/templates/ass.tex"
+    putStrLn $ "read template from "++f++"/.hscripts/templates/ass.tex"
+    case splitter (=='@') datas of 
+        [pretitle, posttitle, preq, midq, postq, end] -> case map readMaybe xs of 
+            [Just a, Just q] -> do
+                let inp = Conf c a q pretitle posttitle preq midq postq end
+                    fo = getFolder f inp
+                createDirectory fo
+                putStrLn $ "made dir "++fo
+                writeFile (fo++"/ass.tex") $ maintext inp
+                putStrLn $ "wrote "++fo++"/ass.tex"
+        _ -> putStrLn "No parse"
