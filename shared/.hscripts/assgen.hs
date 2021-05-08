94a44ae5311fb1334f6624126bf1c7d9326ba0da
sawsuh <prao060@gmail.com>
2021-05-08 20:43:26 +1000

    updated assignment generator even more concise

--- shared/.hscripts/assgen.hs
+++ shared/.hscripts/assgen.hs
@@ -4,9 +4,7 @@ import Text.Read
 import System.Directory
 import Control.Monad
 
-data Conf = Conf { code :: String, na :: Int, nq :: Int, templ :: T.Text }
-container = fst . T.break (=='!') . templ
-qskele = T.drop 1 . snd . T.break (=='!') . templ
+data Conf = Conf { code :: String, na :: Int, nq :: Int, container :: T.Text, qskele :: T.Text }
 title = (.) T.pack $ (++) <$> (++" Assignment ") . code <*> show . na
 qgen n = T.replace (T.pack "@") n . qskele
 qList = T.concat . join (mapM (qgen . T.pack . show) . flip take [1..] . nq)
@@ -16,10 +14,10 @@ getFolder h = (++) <$> (++"ass") . (++) (h++"/school/assignments/") . drop 4 . c
 main = do
     course_code:args <- getArgs
     home_dir <- getHomeDirectory
-    template <- T.pack <$> readFile home_dir ++ "/.hscripts/templates/ass.tex"
+    (atemp, qtemp) <- T.break (=='!') . T.pack <$> (readFile $ home_dir ++ "/.hscripts/templates/ass.tex")
     case map readMaybe args of 
         [Just assign_num, Just q_count] -> do
-            let config = Conf course_code assign_num q_count template
+            let config = Conf course_code assign_num q_count atemp $ T.drop 1 qtemp
                 output_folder = getFolder home_dir config
             createDirectory output_folder
             writeFile (output_folder++"/ass.tex") . T.unpack $ maintext config
