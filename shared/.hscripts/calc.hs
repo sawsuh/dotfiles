e8918195b453381df62581d3799581548e63a23d
Prashant Rao <prao060@gmail.com>
2021-05-10 18:31:08 +1000

    updated to use built in complex type

--- shared/.hscripts/calc.hs
+++ shared/.hscripts/calc.hs
@@ -3,22 +3,15 @@ import Numeric
 import Text.Parsec
 import Control.Monad
 import System.Environment
+import Data.Complex
 
 data Expr = Neg Expr | 
             Sub Expr Expr | 
             Div Expr Expr | 
             Times Expr Expr | 
             Add Expr Expr | 
-            Valc Complex | 
+            Valc (Complex Double) | 
             Exp Expr Expr
-data Complex = Complex Double Double
-instance Show Complex where
-    show (Complex a 0) = showFFloat Nothing a ""
-    show (Complex 0 b) = (showFFloat Nothing b "") ++ "i"
-    show (Complex a b) 
-        | b < 0 = showfloat a ++ " - " ++ showfloat (negate b) ++ "i"
-        | otherwise = showfloat a ++ " + " ++ showfloat b ++ "i"
-        where showfloat = flip (showFFloat Nothing) ""
 indent = unlines . map ("    "++) . lines
 instance Show Expr where
     show (Neg x) = "-\n" ++ (show x)
@@ -26,26 +19,22 @@ instance Show Expr where
     show (Div x y) = indent $ (show x) ++ "/\n" ++ (show y)
     show (Times x y) = indent $ (show x) ++ "*\n" ++ (show y)
     show (Add x y) = indent $ (show x) ++ "+\n" ++ (show y)
-    show (Valc x) = show x ++ "\n"
+    show (Valc x) = show' x ++ "\n"
     show (Exp x y) = indent $ (show x) ++ "^\n" ++ (show y)
+showf = flip (showFFloat Nothing) ""
+show' (0:+b) = (showf b) ++ "i"
+show' (a:+0) = (showf a)
+show' (a:+b)
+    | b < 0 = (showf a) ++ " - " ++ (showf $ negate b) ++ "i"
+    | otherwise = (showf a) ++ " + " ++ (showf b)++"i"
 
-add (Complex a1 b1) (Complex a2 b2) = Complex (a1+a2) (b1+b2)
-times (Complex a1 b1) (Complex a2 b2) = Complex ((a1*a2)-(b1*b2)) ((a1*b2)+(a2*b1))
-neg (Complex a b) = Complex (negate a) (negate b)
-divi (Complex a1 b1) (Complex a2 b2) = Complex ((a1*a2 + b1*b2)/denom) ((a2*b1 - a1*b2)/denom)
-    where denom = a2*a2 + b2*b2
-expo (Complex a b) (Complex c d) = Complex (modulus*(cos arg)) (modulus*(sin arg))
-    where phi = atan2 b a
-          modulus = exp $ ((c/2)*(log (a^2 + b^2)) - (d*phi))
-          arg = (d/2)*(log (a^2+b^2)) + (c*phi)
-
-eval (Add ex1 ex2) = eval ex1 `add` eval ex2
+eval (Add ex1 ex2) = eval ex1 + eval ex2
 eval (Valc x) = x
-eval (Times ex1 ex2) = (eval ex1) `times` (eval ex2)
-eval (Div ex1 ex2) = (eval ex1) `divi` (eval ex2)
-eval (Sub ex1 ex2) = (eval ex1) `add` (neg (eval ex2))
-eval (Neg ex1) = neg (eval ex1)
-eval (Exp ex1 ex2) = (eval ex1) `expo` (eval ex2)
+eval (Times ex1 ex2) = (eval ex1) * (eval ex2)
+eval (Div ex1 ex2) = (eval ex1) / (eval ex2)
+eval (Sub ex1 ex2) = (eval ex1) - (eval ex2)
+eval (Neg ex1) = negate (eval ex1)
+eval (Exp ex1 ex2) = (eval ex1) ** (eval ex2)
 
 exprParser = chainl1 term $ sumParse <|> subParse
 sumParse = Add <$ char '+'
@@ -60,9 +49,9 @@ factor = chainl1 item $ Exp <$ char '^'
 item = brackExpr <|> (fmap Valc $ try parseFloat <|> try parseNumer <|> try parseI)
 
 brackExpr = string "(" *> exprParser <* string ")"
-parseNumer = flip Complex 0 . read <$> many1 digit
-parseI = Complex 0 1 <$ char 'i'
-parseFloat = fmap (flip Complex 0 . read) $ try floatNoLeft <|> try floatLeft
+parseNumer = (:+0) . read <$> many1 digit
+parseI = (0:+1) <$ char 'i'
+parseFloat = fmap ((:+0) . read) $ try floatNoLeft <|> try floatLeft
 
 floatNoLeft = liftM2 (++) ("0." <$ char '.') $ many1 digit
 floatLeft = liftM2 (++) (liftM2 (++) (many1 digit) $ string ".") $ many1 digit
@@ -76,6 +65,6 @@ main = do
     case input of
         Left err -> print err
         Right out -> do
-            print $ eval out
+            putStrLn . show' $ eval out
             putStrLn $ show out ++ "\n"
     main
