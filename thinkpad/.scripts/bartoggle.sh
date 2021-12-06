799f56edc773ba7296881a3e114fc6a24ded5e08
Prashant Rao <prao060@gmail.com>
2021-12-07 09:44:37 +1100

    updated bartoggle script for dualmon

--- thinkpad/.scripts/bartoggle.sh
+++ thinkpad/.scripts/bartoggle.sh
@@ -4,8 +4,8 @@ pid=$(pgrep -f "$cmd")
 wid=$(xdotool search --pid $pid --onlyvisible)
 show=0
 spawn=0
-[[ -z $wid ]] && { bspc config top_padding 70; show=1; }
+[[ -z $wid ]] && { bspc config -m eDP1 top_padding 70; show=1; }
 [[ -z $pid ]] && { show=0; spawn=1; }
 ((show)) && polybar-msg cmd show
 ((spawn)) && eval "$cmd"&>/dev/null&
-((show + spawn)) || { polybar-msg cmd hide; bspc config top_padding 20; }
+((show + spawn)) || { polybar-msg cmd hide; bspc config -m eDP1 top_padding 20; }
