3f56e8fb9a21708192bfd3e5949b252bb4747c9e
sawsuh <prao060@gmail.com>
2023-04-25 20:18:28 +1000

    changed screenshot color to be fixed

--- desktop/.scripts/scrrecthypr.sh
+++ desktop/.scripts/scrrecthypr.sh
@@ -1,5 +1,5 @@
 filename=$(date +'%y-%-m-%-d_%-T')
-grim -g "$(slurp -s '#8d837480' -b '#00000000' -c '#00000000')" $filename.png
+grim -g "$(slurp -b '#8d837480' -s '#00000000' -c '#00000000')" $filename.png
 sleep 1;
 dunstify "screenshot taken"
 at now + 1 hour < <(echo "rm ~/$filename.png")
--- desktop/.scripts/scrrecthyprclip.sh
+++ desktop/.scripts/scrrecthyprclip.sh
@@ -1,3 +1,3 @@
-grim -g "$(slurp -s '#8d837480' -b '#00000000' -c '#00000000')" - | wl-copy
+grim -g "$(slurp -b '#8d837480' -s '#00000000' -c '#00000000')" - | wl-copy
 sleep 1;
 dunstify "screenshot taken"
