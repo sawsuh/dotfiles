d3adb5e7ebf66e8337ddeca1d103414d84bb43d4
Prashant Rao <prao060@gmail.com>
2024-02-29 15:19:24 +1100

    fixed dual monitor ordering

--- thinkpad/.config/bspwm/specific.sh
+++ thinkpad/.config/bspwm/specific.sh
@@ -5,6 +5,7 @@ then
     #xrandr --output eDP1 --right-of HDMI1 --mode 1920x1080 --pos 2560x0 --rotate normal --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal
     bspc monitor eDP1 -d one two three four five 
     bspc monitor HDMI1 -d six seven eight nine ten
+    bspc wm --reorder-monitors eDP1 HDMI1
 else
     bspc monitor -d one two three four five six seven eight nine ten
 fi
@@ -21,3 +22,4 @@ bspc config window_gap          25
 libinput-gestures-setup start&
 pulseaudio --start&
 xss-lock -- bash ~/.lock.sh &
+devmon -gs&
