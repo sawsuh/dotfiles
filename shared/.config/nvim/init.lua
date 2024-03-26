18d4c57f414864c4ef45a4e2e6db237a00654e1d
Prashant Rao <prao060@gmail.com>
2024-03-27 00:20:50 +1100

    added full which-key binding

--- shared/.config/nvim/init.lua
+++ shared/.config/nvim/init.lua
@@ -167,6 +167,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Get keymap help
+vim.keymap.set('n', '<leader>h', '<cmd>WhichKey<CR>', { desc = 'Show keymap [h]elp' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
