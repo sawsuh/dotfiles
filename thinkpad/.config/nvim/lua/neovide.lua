2d3a347c7f614cf37456b12e869a084694c3c81f
Prashant Rao <prao060@gmail.com>
2024-05-09 16:02:12 +1000

    added 'move desktop' to bspwm

--- thinkpad/.config/nvim/lua/neovide.lua
+++ thinkpad/.config/nvim/lua/neovide.lua
@@ -2,6 +2,6 @@
 -- for _, side in ipairs({ "top", "bottom", "right", "left" }) do
 -- 	vim.g["neovide_padding_" .. side] = 20
 -- end
-vim.g.neovide_scale_factor = 0.5
-vim.opt.linespace = 3
+vim.g.neovide_scale_factor = 0.8
+vim.opt.linespace = 5
 -- vim.g.neovide_cursor_animation_length = 0
