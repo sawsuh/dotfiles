9bcb7d247b00a7d3a3564694f6a85c4bb38d1879
sawsuh <prao060@gmail.com>
2024-06-02 23:06:58 +1000

    updating thinkpad files

---
+++ thinkpad/.config/nvim/lua/neovide.lua
@@ -0,0 +1,6 @@
+vim.opt.linespace = 10
+vim.g.neovide_scale_factor = 0.65
+-- vim.g.neovide_cursor_animation_length = 0
+-- for _, side in ipairs({ "top", "bottom", "right", "left" }) do
+-- 	vim.g["neovide_padding_" .. side] = 50
+-- end
